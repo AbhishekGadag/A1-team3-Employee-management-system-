7edcf9034eaa3edb59a71c617b3955cdfb59cb26
Abhishek Gadag <47495127+AbhishekGadag@users.noreply.github.com>
2021-05-11 15:38:16 +0530

    Delete backend directory

--- backend/models/mongoDB/leavecode.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const leaveCodeSchema = new Schema({
-    code: String,
-    descr: String,
-});
-
-module.exports = mongoose.model('LeaveCode', leaveCodeSchema);
